d38816d9f95fce3f62348f77a326eb3fd73bb51d
TJ Kells <tjkells@gmail.com>
2012-04-12 23:54:46 -0500

    removed some left over junk from testing

--- index.py
+++ index.py
@@ -11,10 +11,7 @@ app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {
 
 @app.route("/", methods=['POST','GET'])
 def index():
-	if request.method == 'POST':
-		return render_template('index.html', data=request.form)
-	else:
-		return render_template('index.html')
+	return render_template('index.html')
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0',debug=True)
